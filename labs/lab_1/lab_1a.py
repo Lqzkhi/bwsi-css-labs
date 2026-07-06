9f48f3e18a18fb7c6bcf658c43b120afccc74616
Brandon Jia <bljia2020@gmail.com>
2026-07-06 13:53:02 -0400

    Add introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Brandon Jia" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Introduction")
 if __name__ == "__main__":
     main()
